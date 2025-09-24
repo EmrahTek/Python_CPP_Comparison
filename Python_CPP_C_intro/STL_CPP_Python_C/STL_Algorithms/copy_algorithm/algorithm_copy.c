bfc3d7a52eb551aa404d83f1dc4db2affb187c76
Emrah Tekin <swissemrah@gmail.com>
2025-09-24 16:18:13 +0200

    eksik kisim yazildi c de yazildi

--- Python_CPP_C_intro/STL_CPP_Python_C/STL_Algorithms/copy_algorithm/algorithm_copy.c
+++ Python_CPP_C_intro/STL_CPP_Python_C/STL_Algorithms/copy_algorithm/algorithm_copy.c
@@ -15,4 +15,36 @@ memcpy veya for döngüsü ile kopyalama kullanılır.
 int main(){
     double v[4] = {1,2,0,0};
     // İlk 2 elemanı (v[0], v[1]) -> v[2], v[3] konumlarına kopyala
+    memcpy(&v[2], &v[0], 2*sizeof(double)); 
+
+    // v yazdir
+    for(size_t i = 0; i < 4; i++){
+        printf("%.1f", v[i]);
+    }
+    printf("\n");
+
+    // w dizisi
+    double w[2] = {0, 0};
+
+    // w yazdır
+    for (size_t i = 0; i < 2; i++) {
+        printf("%.1f ", w[i]);
+    }
+    printf("\n");
+
+    return 0;
+
+    /*
+    memcpy nedir?
+
+    memcpy (memory copy) C’nin <string.h> kütüphanesinden gelen bir fonksiyondur.
+
+    Bellekte bir kaynaktan belirli sayıda baytı alıp hedef adrese kopyalar.
+
+    Yani, "şu kadar byte’ı buradan al, şuraya yapıştır" der.
+    
+    
+    */
+
+
 }
